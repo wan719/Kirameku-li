b8416208f14653b7b9989b18c0db646390442b2b
WAN719 <3425446714@qq.com>
2026-06-01 17:30:42 +0800

    更改数据库，改为MySQL

--- Kirameku-backend/app/api/comments.py
+++ Kirameku-backend/app/api/comments.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from fastapi import APIRouter, Depends, Query, Request
 from sqlmodel import Session
 
--- Kirameku-backend/app/services/comment_service.py
+++ Kirameku-backend/app/services/comment_service.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from sqlmodel import Session, select
 from fastapi import HTTPException
 
